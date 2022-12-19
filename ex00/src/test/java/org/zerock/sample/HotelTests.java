6c0fd8335a3c7184f4cf4a087a3d30c00fa1c99e
Janghyerim <114923517+Janghyerim@users.noreply.github.com>
2022-12-19 18:17:40 +0900

    Add files via upload

---
+++ ex00/src/test/java/org/zerock/sample/HotelTests.java
@@ -0,0 +1,27 @@
+//package org.zerock.sample;
+//
+//import org.junit.runner.RunWith;
+//import org.springframework.test.context.ContextConfiguration;
+//import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+//
+//import jdk.internal.org.jline.utils.Log;
+//import lombok.extern.log4j.Log4j;
+//
+//
+//@RunWith(SpringJUnit4ClassRunner.class)
+//@ContextConfiguration("file:src/main/webapp/WEB-INF/spring/root-context.xml")
+//@Log4j
+//public class HotelTests {
+//	
+//	@Setter(onMethod_ = {@Autowired})
+//	private SampleHotel hotel;
+//	
+//	@Test
+//	public void testExist() {
+//		assertNotNull(hotel);
+//		
+//		Log.info(hotel);
+//		Log.info("---------------------------");
+//		Log.info(hotel.getChef());
+//	}
+//}
